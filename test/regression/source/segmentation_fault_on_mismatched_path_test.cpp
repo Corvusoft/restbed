77a67c79a6eb46a739886846dd12967f0400bb47
Ben Crowhurst <bcrowhurst@Bens-iMac.local>
2013-11-27 22:11:36 +0000

    Regression testcase for segfault when not matching a resource.

---
+++ test/regression/source/segmentation_fault_on_mismatched_path_test.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2013 Corvusoft
+ */
+
+#ifdef __APPLE__
+#define GTEST_USE_OWN_TR1_TUPLE 1 //See issue tracker #11
+#endif
+
+//System Includes
+#include <memory>
+#include <functional>
+
+//Project Includes
+#include <restbed>
+#include "helpers/http.h"
+#include "fixtures/service_fixture.h"
+
+//External Includes
+#include <gtest/gtest.h>
+
+//System Namespaces
+using std::bind;
+using std::shared_ptr;
+using std::placeholders::_1;
+
+//Project Namespaces
+using namespace restbed;
+
+//External Namespaces
+
+TEST_F( ServiceFixture, mismatched_resource_path )
+{
+	int status_code = Http::get( "http://localhost:1984/" );
+	
+	EXPECT_TRUE( status_code == 404 );
+}
+
+TEST_F( ServiceFixture, matched_resource_path )
+{
+	int status_code = Http::get( "http://localhost:1984/test" );
+	
+	EXPECT_TRUE( status_code == 200 );
+}
