1724b8e043d89ae0c897933217f659aa94c29002
corvusoft <bcrowhurst@Bens-iMac.local>
2014-03-12 20:52:17 +0000

    legacy site removed.

--- site/script/router.js
+++
@@ -1,8 +0,0 @@
-Application.Router.map(function() {
-	var router = this;
-
-	router.route('index', { path: '/'} );
-	router.route('api');
-	router.route('features');
-	router.route('issues');
-});
--- site/script/view/api_content_view.js
+++
@@ -1,9 +0,0 @@
-Application.ApiContent = Ember.View.extend({
-    templateName: 'uml',
-    updateTemplate: function(templateName) {
-        console.log(templateName);
-        var view = this;
-        view.set('templateName', templateName);
-        view.rerender();
-    }
-});
