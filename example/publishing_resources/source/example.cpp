f2ca61433d49b6cb7bf4d0b967615ff7dab78af3
corvusoft <bcrowhurst@Bens-iMac.local>
2014-03-15 13:34:21 +0000

    created resource publishing example

---
+++ example/publishing_resources/source/example.cpp
@@ -0,0 +1,30 @@
+#include "restbed"
+
+using namespace restbed;
+
+Response get_method_handler( const Request& );
+
+int main( int, char** )
+{
+    Resource resource;
+    resource.set_path( "/resource" );
+    resource.set_method_handler( "GET", &get_method_handler );
+
+    Settings settings;
+    settings.set_port( 1984 );
+
+    Service service( settings );
+    service.publish( resource );
+    service.start( );
+
+    return EXIT_SUCCESS;
+}
+
+Response get_method_handler( const Request& )
+{
+    Response response;
+    response.set_body( "Hello, World!" );
+    response.set_status_code( StatusCode::OK );
+
+    return response;
+}
