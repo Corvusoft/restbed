02a5d1177f4ee8466f39ba2a6d567c1ace791daa
corvusoft <bcrowhurst@Bens-iMac.local>
2014-03-17 14:22:05 +0000

    Created path parameter and request filtering examples

---
+++ example/path_parameters/source/example.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include "restbed"
+
+using namespace std;
+using namespace restbed;
+
+Response get_method_handler( const Request& );
+
+int main( int, char** )
+{
+    Resource resource;
+    resource.set_path( "/resource/{name: .*}" );
+    resource.set_method_handler( "GET", &get_method_handler );
+
+    Settings settings;
+    settings.set_port( 1984 );
+
+    Service service( settings );
+    service.publish( resource );
+    service.start( );
+
+    return EXIT_SUCCESS;
+}
+
+Response get_method_handler( const Request& request )
+{
+    string body = "Hello, ";
+    body += request.get_path_parameter( "name" ) + "!";
+
+    Response response;
+    response.set_body( body );
+    response.set_status_code( StatusCode::OK );
+
+    return response;
+}
---
+++ example/request_filtering/source/example.cpp
@@ -0,0 +1,49 @@
+#include "restbed"
+
+using namespace restbed;
+
+Response get_xml_method_handler( const Request& );
+Response get_json_method_handler( const Request& );
+
+int main( int, char** )
+{
+    Resource xml;
+    xml.set_path( "/resource" );
+    xml.set_header_filter( "Accept", "application/xml" );
+    xml.set_method_handler( "GET", &get_xml_method_handler );
+
+    Resource json;
+    json.set_path( "/resource" );
+    json.set_header_filter( "Accept", "application/json" );
+    json.set_method_handler( "GET", &get_json_method_handler );
+
+    Settings settings;
+    settings.set_port( 1984 );
+
+    Service service( settings );
+    service.publish( xml );
+    service.publish( json );
+    service.start( );
+
+    return EXIT_SUCCESS;
+}
+
+Response get_xml_method_handler( const Request& )
+{
+    Response response;
+    response.set_body( "<hello><world></world></hello>" );
+    response.set_header( "Content-Type", "application/xml" );
+    response.set_status_code( StatusCode::OK );
+
+    return response;
+}
+
+Response get_json_method_handler( const Request& )
+{
+    Response response;
+    response.set_body( "{ \"Hello\": \", World!\" }" );
+    response.set_header( "Content-Type", "application/json" );
+    response.set_status_code( StatusCode::OK );
+
+    return response;
+}
