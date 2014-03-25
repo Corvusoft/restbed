63fd87936e2267d359ba0be461e0a6a56c5284fa
Craig Lilley <cralilley@gmail.com>
2014-03-25 23:27:14 +0000

    Fixed error in CMakeLists.txt.
    Include directories were not being set correctly for dependencies other than ASIO.
    Dependencies now added on an individual basis.

--- build/Modules/Findasio.cmake
+++ build/Modules/Findasio.cmake
@@ -3,14 +3,21 @@
 find_path( ASIO_INCLUDE_DIR asio.hpp "/usr/include" "/usr/local/include" )
 
 if ( ASIO_INCLUDE_DIR )
+
     set( ASIO_FOUND TRUE )
+
 endif ( ASIO_INCLUDE_DIR )
 
 if ( ASIO_FOUND )
+
     if ( NOT ASIO_FIND_QUIETLY )
 	message( STATUS "Found asio.hpp header file: ${ASIO_INCLUDE_DIR}" )
     endif ( NOT ASIO_FIND_QUIETLY )
+
+    set(ASIO_INCLUDE_DIRS ${ASIO_INCLUDE_DIR} )
+
 else ( ASIO_FOUND )
+
     if ( ASIO_FIND_REQUIRED )
 	if ( NOT ASIO_INCLUDE_DIR )
 	    message( FATAL_ERROR "Could not find asio.hpp header file!" )
