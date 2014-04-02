a5804fd4879e687cdba7a89cfbba3aab4300049e
corvusoft <bcrowhurst@Bens-iMac.local>
2014-04-02 20:17:00 +0100

    Created regression test to check for valid regex support.

---
+++ test/regression/source/missing_regex_support_on_gcc_4.8.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2013, 2014 Corvusoft
+ */
+
+/*
+ * See issue tracker bug #9 for details.
+ */
+
+#ifdef __APPLE__
+#define GTEST_USE_OWN_TR1_TUPLE 1 //See issue tracker #11
+#endif
+
+//System Includes
+#include <regex>
+
+//Project Includes
+
+//External Includes
+#include <gtest/gtest.h>
+
+//System Namespaces
+
+//Project Namespaces
+using std::regex;
+
+//External Namespaces
+
+TEST( Service, missing_regex_support )
+{
+    ASSERT_NO_THROW( regex( "(abc[1234])" ) );
+}
