fb835ab0332523c2d67d833653fb57a0718b77b4
Ben <Crowhurst@Bens-MacBook-Pro.local>
2014-06-04 13:15:01 -0400

    Removed automatic building of dependencies.

--- cmake/FindGTest.cmake
+++ cmake/FindGTest.cmake
@@ -1,23 +1,23 @@
 # Copyright (c) 2013, 2014 Corvusoft
 
-find_path( GTest_SOURCE_DIR "CMakeLists.txt" "${CMAKE_SOURCE_DIR}/dependency/gtest" )
-find_path( GTest_INCLUDE_DIR "gtest.h" "${GTest_SOURCE_DIR}/include/gtest")
-find_path( GTest_CC_FILE "gtest.cc" "${GTest_SOURCE_DIR}/src")
+set( GTest_SOURCE_DIR "${CMAKE_SOURCE_DIR}/dependency/gtest" )
 
-if ( NOT GTest_INCLUDE_DIR OR NOT GTest_SOURCE_DIR OR NOT GTest_CC_FILE )
+find_path( GTest_INCLUDE_DIR "gtest.h" "${GTest_SOURCE_DIR}/include/gtest" )
+find_path( GTest_LIBRARY "libgtest.a" "${GTest_SOURCE_DIR}" )
+find_path( GTest_MAIN_LIBRARY "libgtest_main.a" "${GTest_SOURCE_DIR}" )
 
+if ( NOT GTest_INCLUDE_DIR OR NOT GTest_LIBRARY OR NOT GTest_MAIN_LIBRARY )
     if ( GTest_FIND_REQUIRED )
         message( FATAL_ERROR "Could not find gtest source directory or the directory has missing files." )
     endif ( GTest_FIND_REQUIRED )
 
-else( )
-
+else ( )
     if ( NOT GTest_FIND_QUIETLY )
         message( STATUS "Found gtest: ${GTest_SOURCE_DIR}" )
-    endif ( NOT  GTest_FIND_QUIETLY )
+    endif ( NOT GTest_FIND_QUIETLY )
 
     set( GTest_FOUND TRUE )
     set( GTest_INCLUDE_DIR "${GTest_SOURCE_DIR}/include" )
-
+    set( GTest_LIBRARY "${GTest_SOURCE_DIR}/libgtest.a" )
+    set( GTest_MAIN_LIBRARY "${GTest_SOURCE_DIR}/libgtest_main.a" )
 endif ( )
-
