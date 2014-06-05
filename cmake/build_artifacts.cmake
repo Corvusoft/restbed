5739fefe725109cd35925f7191ddba6a8ac9fea5
Ben <Crowhurst@Bens-MacBook-Pro.local>
2014-06-05 09:06:42 -0400

    Formatting.

--- cmake/build_artifacts.cmake
+++ cmake/build_artifacts.cmake
@@ -4,30 +4,31 @@
 # Install artifacts
 #
 set( FRAMEWORK_ARTIFACTS
-	${FRAMEWORK_SOURCE_DIR}/byte
-	${FRAMEWORK_SOURCE_DIR}/byte.h
-	${FRAMEWORK_SOURCE_DIR}/bytes
-	${FRAMEWORK_SOURCE_DIR}/bytes.h
-	${FRAMEWORK_SOURCE_DIR}/mode
-	${FRAMEWORK_SOURCE_DIR}/mode.h
-	${FRAMEWORK_SOURCE_DIR}/settings
-	${FRAMEWORK_SOURCE_DIR}/settings.h
-	${FRAMEWORK_SOURCE_DIR}/log_level
-	${FRAMEWORK_SOURCE_DIR}/log_level.h
-	${FRAMEWORK_SOURCE_DIR}/status_code
-	${FRAMEWORK_SOURCE_DIR}/status_code.h
-	${FRAMEWORK_SOURCE_DIR}/method
-	${FRAMEWORK_SOURCE_DIR}/method.h
-	${FRAMEWORK_SOURCE_DIR}/resource
-	${FRAMEWORK_SOURCE_DIR}/resource.h
-	${FRAMEWORK_SOURCE_DIR}/request
-	${FRAMEWORK_SOURCE_DIR}/request.h
-	${FRAMEWORK_SOURCE_DIR}/response
-	${FRAMEWORK_SOURCE_DIR}/response.h
-	${FRAMEWORK_SOURCE_DIR}/service
-	${FRAMEWORK_SOURCE_DIR}/service.h
-	${FRAMEWORK_SOURCE_DIR}/logger
-	${FRAMEWORK_SOURCE_DIR}/logger.h
-	${FRAMEWORK_SOURCE_DIR}/log_level
-	${FRAMEWORK_SOURCE_DIR}/log_level.h
+     ${FRAMEWORK_SOURCE_DIR}/byte
+     ${FRAMEWORK_SOURCE_DIR}/byte.h
+     ${FRAMEWORK_SOURCE_DIR}/bytes
+     ${FRAMEWORK_SOURCE_DIR}/bytes.h
+     ${FRAMEWORK_SOURCE_DIR}/mode
+     ${FRAMEWORK_SOURCE_DIR}/mode.h
+     ${FRAMEWORK_SOURCE_DIR}/settings
+     ${FRAMEWORK_SOURCE_DIR}/settings.h
+     ${FRAMEWORK_SOURCE_DIR}/log_level
+     ${FRAMEWORK_SOURCE_DIR}/log_level.h
+     ${FRAMEWORK_SOURCE_DIR}/status_code
+     ${FRAMEWORK_SOURCE_DIR}/status_code.h
+     ${FRAMEWORK_SOURCE_DIR}/method
+     ${FRAMEWORK_SOURCE_DIR}/method.h
+     ${FRAMEWORK_SOURCE_DIR}/resource
+     ${FRAMEWORK_SOURCE_DIR}/resource.h
+     ${FRAMEWORK_SOURCE_DIR}/request
+     ${FRAMEWORK_SOURCE_DIR}/request.h
+     ${FRAMEWORK_SOURCE_DIR}/response
+     ${FRAMEWORK_SOURCE_DIR}/response.h
+     ${FRAMEWORK_SOURCE_DIR}/service
+     ${FRAMEWORK_SOURCE_DIR}/service.h
+     ${FRAMEWORK_SOURCE_DIR}/logger
+     ${FRAMEWORK_SOURCE_DIR}/logger.h
+     ${FRAMEWORK_SOURCE_DIR}/log_level
+     ${FRAMEWORK_SOURCE_DIR}/log_level.h
 )
+
