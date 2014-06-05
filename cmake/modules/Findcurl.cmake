a9f3d9a700c02f2a8970fe267cd35a9d918e6504
Ben <Crowhurst@Bens-MacBook-Pro.local>
2014-06-05 09:43:16 -0400

    Tabs to 4 Spaces

--- cmake/modules/Findcurl.cmake
+++ cmake/modules/Findcurl.cmake
@@ -6,13 +6,13 @@ find_library( curl_LIBRARY_PATH NAMES curl )
 find_path( curl_INCLUDE_DIR NAMES curl/curl.h )
 
 if( curl_LIBRARY_PATH AND curl_INCLUDE_DIR )
-	set( curl_FOUND TRUE )
+    set( curl_FOUND TRUE )
 
-	get_filename_component( curl_LIBRARY_DIR "${curl_LIBRARY_PATH}" PATH )
-	get_filename_component( curl_LIBRARY "${curl_LIBRARY_PATH}" NAME )
+    get_filename_component( curl_LIBRARY_DIR "${curl_LIBRARY_PATH}" PATH )
+    get_filename_component( curl_LIBRARY "${curl_LIBRARY_PATH}" NAME )
 
-	message( STATUS "Found curl: ${curl_LIBRARY} ${curl_INCLUDE_DIR}/curl/curl.h" )
+    message( STATUS "Found curl: ${curl_LIBRARY} ${curl_INCLUDE_DIR}/curl/curl.h" )
 else( )
-	message( FATAL_ERROR "Could not find curl." )
+    message( FATAL_ERROR "Could not find curl." )
 endif ( )
 
