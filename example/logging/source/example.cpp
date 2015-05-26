0f1c1c681bb1ee677a1708599af39cf6593ece86
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2015-05-26 18:23:19 +0100

    completed logging example refactor

--- example/logging/source/example.cpp
+++ example/logging/source/example.cpp
@@ -1,35 +1,69 @@
 #include <memory>
 #include <cstdlib>
-
-#include "restbed"
+#include <restbed>
 
 using namespace std;
 using namespace restbed;
 
-Response get_method_handler( const Request& )
+class CustomLogger : public Logger
 {
-    Response response;
-    response.set_body( "Hello, World!" );
-    response.set_status_code( StatusCode::OK );
-    
-    return response;
+public:
+    void stop( void )
+    {
+        return;
+    }
+
+    void start( const shared_ptr< Settings >& )
+    {
+        return;
+    }
+
+    void log( const Level, const string format, ... )
+    {
+        va_list arguments;
+
+        va_start( arguments, format );
+
+        vprintf( format.data( ), arguments );
+
+        va_end( arguments );
+    }
+
+    void log_if( bool expression, const Level level, const string format, ... )
+    {
+        if ( expression )
+        {
+            va_list arguments;
+
+            va_start( arguments, format );
+
+            log( level, format, arguments );
+
+            va_end( arguments );
+        }
+    }
+};
+
+void get_method_handler( const shared_ptr< Session >& session )
+{
+    session->close( OK, "Hello, World!", { { "Content-Length", "13" } } );
 }
 
 int main( const int, const char** )
 {
-    Resource resource;
-    resource.set_path( "/resource" );
-    resource.set_method_handler( "GET", &get_method_handler );
+    auto resource = make_shared< Resource >( );
+    resource->set_path( "/resource" );
+    resource->set_method_handler( "GET", &get_method_handler );
+
+    auto settings = make_shared< Settings >( );
+    settings->set_port( 1984 );
+    settings->set_default_header( "Connection", "close" );
     
-    auto logger = make_shared< Logger >( );
-    
-    Settings settings;
-    settings.set_port( 1984 );
-    
-    Service service( settings );
+    Service service;
     service.publish( resource );
-    service.set_logger( logger );
-    service.start( );
+    service.set_logger( make_shared< CustomLogger >( ) );
+
+    service.start( settings );
     
     return EXIT_SUCCESS;
 }
