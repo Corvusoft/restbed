2a15e7dc0cd25201845d93af716bf2bcf25bcb97
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2015-06-19 13:36:12 +0100

    completed deflate compression example

--- example/compression/source/example.cpp
+++ example/compression/source/example.cpp
@@ -1,3 +1,13 @@
+/*
+ * Example illustrating the use of deflate compression (RFC 1950-1952).
+ *
+ * Server Usage:
+ *    ./distribution/example/compression
+ *
+ * Client Usage:
+ *    curl -v -H"Content-Encoding: deflate" -X POST --data-binary @'distribution/resource/data.zlib' 'http://localhost:1984/api/deflate'
+ */
+
 #include <map>
 #include <memory>
 #include <cstdlib>
@@ -10,7 +20,7 @@ using namespace std;
 using namespace restbed;
 using namespace framework;
 
-void unzip_method_handler( const shared_ptr< Session >& session )
+void deflate_method_handler( const shared_ptr< Session >& session )
 {
     const auto request = session->get_request( );
 
@@ -21,15 +31,15 @@ void unzip_method_handler( const shared_ptr< Session >& session )
     {
         Bytes result = body;
 
-        if ( request->get_header( "Content-Encoding", String::lowercase ) == "gzip" )
+        if ( request->get_header( "Content-Encoding", String::lowercase ) == "deflate" )
         {
-            size_t length = body.size( );
-            unsigned char* data = new unsigned char[ length + 1 ];
-            const int status = uncompress( data, &length, body.data( ), length );
+            size_t length = 15;
+            unsigned char* data = new unsigned char[ length ];
+            const int status = uncompress( data, &length, body.data( ), body.size( ) );
 
             if ( status not_eq MZ_OK )
             {
-                const auto message = String::format( "Failed to unzip: %s\n", mz_error( status ) );
+                const auto message = String::format( "Failed to deflate: %s\n", mz_error( status ) );
                 session->close( 400, message, { { "Content-Length", ::to_string( message.length( ) ) }, { "Content-Type", "text/plain" } } );
                 delete[ ] data;
                 return;
@@ -39,15 +49,15 @@ void unzip_method_handler( const shared_ptr< Session >& session )
             delete[ ] data;
         }
 
-        session->close( 200, result, { { "Content-Length", ::to_string( body.size( ) ) }, { "Content-Type", request->get_header( "Content-Type" ) } } );
+        session->close( 200, result, { { "Content-Length", ::to_string( result.size( ) ) }, { "Content-Type", "text/plain" } } );
     } );
 }
 
 int main( const int, const char** )
 {
     auto resource = make_shared< Resource >( );
-    resource->set_path( "/api/unzip" );
-    resource->set_method_handler( "POST", &unzip_method_handler );
+    resource->set_path( "/api/deflate" );
+    resource->set_method_handler( "POST", &deflate_method_handler );
 
     auto settings = make_shared< Settings >( );
     settings->set_port( 1984 );
