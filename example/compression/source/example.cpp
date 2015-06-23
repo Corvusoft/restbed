a63ae593ff0704ff54b37d3024251f83f2eb9091
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2015-06-23 17:05:28 +0100

    removed hardcoded size for decompressed data buffers.

--- example/compression/source/example.cpp
+++ example/compression/source/example.cpp
@@ -23,20 +23,20 @@ using namespace framework;
 void deflate_method_handler( const shared_ptr< Session >& session )
 {
     const auto request = session->get_request( );
-
+    
     size_t content_length = 0;
     request->get_header( "Content-Length", content_length );
-
-    session->fetch( content_length, [ request ]( const shared_ptr< Session >& session, const Bytes& body )
+    
+    session->fetch( content_length, [ request ]( const shared_ptr< Session >& session, const Bytes & body )
     {
         Bytes result = body;
-
+        
         if ( request->get_header( "Content-Encoding", String::lowercase ) == "deflate" )
         {
-            size_t length = 15;
+            size_t length = compressBound( body.size( ) );
             unsigned char* data = new unsigned char[ length ];
             const int status = uncompress( data, &length, body.data( ), body.size( ) );
-
+            
             if ( status not_eq MZ_OK )
             {
                 const auto message = String::format( "Failed to deflate: %s\n", mz_error( status ) );
@@ -44,11 +44,11 @@ void deflate_method_handler( const shared_ptr< Session >& session )
                 delete[ ] data;
                 return;
             }
-
+            
             result = Bytes( data, data + length );
             delete[ ] data;
         }
-
+        
         session->close( 200, result, { { "Content-Length", ::to_string( result.size( ) ) }, { "Content-Type", "text/plain" } } );
     } );
 }
@@ -58,11 +58,11 @@ int main( const int, const char** )
     auto resource = make_shared< Resource >( );
     resource->set_path( "/api/deflate" );
     resource->set_method_handler( "POST", deflate_method_handler );
-
+    
     auto settings = make_shared< Settings >( );
     settings->set_port( 1984 );
     settings->set_default_header( "Connection", "close" );
-
+    
     Service service;
     service.publish( resource );
     service.start( settings );
