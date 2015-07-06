b5ca0edf22851a28fe91ae27b5bb72ca630519c2
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2015-07-06 16:47:17 +0100

    resolved #11, parallel builds.

--- cmake/modules/Findframework.cmake
+++ cmake/modules/Findframework.cmake
@@ -8,13 +8,16 @@ if ( framework_SOURCE )
     set( framework_DISTRIBUTION "${CMAKE_CURRENT_BINARY_DIR}/framework_build/distribution" )
 
     include( ExternalProject )
-    ExternalProject_Add( framework SOURCE_DIR ${framework_SOURCE}
+    ExternalProject_Add( corvusoft-framework SOURCE_DIR ${framework_SOURCE}
                          PREFIX framework_build
                          INSTALL_DIR ${framework_DISTRIBUTION}
                          CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${framework_DISTRIBUTION} )
 
     set( framework_INCLUDE "${framework_DISTRIBUTION}/include" )
     set( framework_LIBRARY "${framework_DISTRIBUTION}/library/${CMAKE_SHARED_LIBRARY_PREFIX}framework${CMAKE_SHARED_LIBRARY_SUFFIX}" )
+
+    add_library( framework SHARED IMPORTED )
+    set_target_properties( framework PROPERTIES IMPORTED_LOCATION ${framework_LIBRARY} )
 
     install( FILES ${framework_LIBRARY} DESTINATION "library" )
     install( FILES "${framework_INCLUDE}/framework" DESTINATION "include" )
