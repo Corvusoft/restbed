1206728fffa1ead761e279e96bb666d5be994053
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2015-08-08 19:32:05 +0100

    silenced linux build warnings within examples.

--- example/compression/source/example.cpp
+++ example/compression/source/example.cpp
@@ -15,6 +15,7 @@
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wextra"
+#pragma GCC diagnostic ignored "-Weffc++"
 #pragma GCC diagnostic ignored "-Wstrict-aliasing"
 #include "miniz.h"
 #pragma GCC diagnostic pop
