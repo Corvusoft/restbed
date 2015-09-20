652168afda2619429f524e9d44585ee960ebc207
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2015-09-20 12:19:15 +0100

    Improved SessionManager thread safety.

--- example/session_manager/source/example.cpp
+++ example/session_manager/source/example.cpp
@@ -10,6 +10,7 @@
  */
 
 #include <map>
+#include <mutex>
 #include <chrono>
 #include <string>
 #include <memory>
@@ -22,97 +23,104 @@ using namespace restbed;
 
 class InMemorySessionManager : public SessionManager
 {
-public:
-	InMemorySessionManager( void ) : sessions( )
-    {
-        return;
-    }
-    
-    ~InMemorySessionManager( void )
-    {
-        return;
-    }
-    
-    void start( const shared_ptr< const Settings >& )
-    {
-        return;
-    }
-    
-    void create( const function< void ( const shared_ptr< Session > ) >& callback )
-    {
-        static const string charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        static uniform_int_distribution< > selector( 0, charset.size( ) - 1 );
-        static mt19937 generator( chrono::high_resolution_clock::now( ).time_since_epoch( ).count( ) );
-        
-        string key = "";
-        for ( int index = 0; index < 32; index++ )
+    public:
+        InMemorySessionManager( void ) : sessions( )
         {
-            key += charset.at( selector( generator ) );
+            return;
         }
-
-        auto session = make_shared< Session >( key );
         
-        callback( session );
-    }
-    
-    void load( const shared_ptr< Session > session, const function< void ( const shared_ptr< Session > ) >& callback )
-    {
-        const auto request = session->get_request( );
-        auto previous_session = sessions.find( request->get_header( "SessionID" ) );
-
-        if ( previous_session not_eq sessions.end( ) )
+        ~InMemorySessionManager( void )
         {
-            const auto key = previous_session->second->get_id( );
-            session->set_id( key );
-            session->set_header( "SessionID", key );
-
-            for ( const auto key : previous_session->second->keys( ) )
+            return;
+        }
+        
+        void start( const shared_ptr< const Settings >& )
+        {
+            return;
+        }
+        
+        void create( const function< void ( const shared_ptr< Session > ) >& callback )
+        {
+            static const string charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            static uniform_int_distribution< > selector( 0, charset.size( ) - 1 );
+            static mt19937 generator( chrono::high_resolution_clock::now( ).time_since_epoch( ).count( ) );
+            
+            string key = "";
+            
+            for ( int index = 0; index < 32; index++ )
             {
-                session->set( key, previous_session->second->get( key ) );
+                key += charset.at( selector( generator ) );
             }
-        }
-        else
-        {
-            const auto key = session->get_id( );
-            session->set_id( key );
-            session->set_header( "SessionID", key );
+            
+            auto session = make_shared< Session >( key );
+            
+            callback( session );
         }
         
-        callback( session );
-    }
-
-    void save( const shared_ptr< Session > session, const function< void ( const shared_ptr< Session > ) >& callback )
-    {
-        sessions[ session->get_id( ) ] = session;
-        callback( session );
-    }
-
-    void stop( void )
-    {
-        return;
-    }
-
-private:
-	map< string, shared_ptr< Session > > sessions;
+        void load( const shared_ptr< Session > session, const function< void ( const shared_ptr< Session > ) >& callback )
+        {
+            lock_guard< mutex > lock( sessions_lock );
+            
+            const auto request = session->get_request( );
+            auto previous_session = sessions.find( request->get_header( "SessionID" ) );
+            
+            if ( previous_session not_eq sessions.end( ) )
+            {
+                const auto key = previous_session->second->get_id( );
+                session->set_id( key );
+                session->set_header( "SessionID", key );
+                
+                for ( const auto key : previous_session->second->keys( ) )
+                {
+                    session->set( key, previous_session->second->get( key ) );
+                }
+            }
+            else
+            {
+                const auto key = session->get_id( );
+                session->set_id( key );
+                session->set_header( "SessionID", key );
+            }
+            
+            callback( session );
+        }
+        
+        void save( const shared_ptr< Session > session, const function< void ( const shared_ptr< Session > ) >& callback )
+        {
+            lock_guard< mutex > lock( sessions_lock );
+            
+            sessions[ session->get_id( ) ] = session;
+            callback( session );
+        }
+        
+        void stop( void )
+        {
+            return;
+        }
+        
+    private:
+        mutex sessions_lock;
+        
+        map< string, shared_ptr< Session > > sessions;
 };
 
 void get_method_handler( const shared_ptr< Session > session )
 {
     string body = "Previous Session Data\n";
-
+    
     for ( const auto key : session->keys( ) )
     {
         string value = session->get( key );
         body += key + "=" + value + "\n";
     }
-
-    const auto request = session->get_request( ); 
-
+    
+    const auto request = session->get_request( );
+    
     for ( const auto query_parameter : request->get_query_parameters( ) )
     {
         session->set( query_parameter.first, query_parameter.second );
     }
-
+    
     session->close( OK, body, { { "Connection", "close" } } );
 }
 
