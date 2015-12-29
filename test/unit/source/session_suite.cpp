6ca3b7ea9f5d653e14cfe40c2baeb726c489a5c2
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2015-12-29 14:12:26 +1300

    Resolved issue with session unit test suite.

--- test/unit/source/session_suite.cpp
+++ test/unit/source/session_suite.cpp
@@ -35,79 +35,79 @@ TEST_CASE( "validate default instance values", "[session]" )
     REQUIRE( session.is_closed( ) == true );
     REQUIRE( session.get_origin( ) == "" );
     REQUIRE( session.get_destination( ) == "" );
-    //REQUIRE( session.get_headers( ).empty( ) );
-    //REQUIRE( session.get_id( ) == "f47ac10b-58cc-4372-a567-0e02b2c3d479" );
+    REQUIRE( session.get_headers( ).empty( ) );
+    REQUIRE( session.get_id( ) == "f47ac10b-58cc-4372-a567-0e02b2c3d479" );
 }
 
-// TEST_CASE( "confirm empty session id throws no exceptions", "[session]" )
-// {
-//     REQUIRE_NOTHROW( Session( "" ) );
-// }
+TEST_CASE( "confirm empty session id throws no exceptions", "[session]" )
+{
+    REQUIRE_NOTHROW( Session( "" ) );
+}
 
-// TEST_CASE( "confirm default destructor throws no exceptions", "[session]" )
-// {
-//     auto session = new Session( "f47ac10b-58cc-4372-a567-0e02b2c3d479" );
+TEST_CASE( "confirm default destructor throws no exceptions", "[session]" )
+{
+    auto session = new Session( "f47ac10b-58cc-4372-a567-0e02b2c3d479" );
     
-//     REQUIRE_NOTHROW( delete session );
-// }
+    REQUIRE_NOTHROW( delete session );
+}
 
-// TEST_CASE( "validate setters modify default values", "[session]" )
-// {
-//     Session session( "" );
-//     session.set_id( "f47ac10b-58cc-4372-a567-0e02b2c3d479" );
-//     REQUIRE( session.get_id( ) == "f47ac10b-58cc-4372-a567-0e02b2c3d479" );
+TEST_CASE( "validate setters modify default values", "[session]" )
+{
+    Session session( "" );
+    session.set_id( "f47ac10b-58cc-4372-a567-0e02b2c3d479" );
+    REQUIRE( session.get_id( ) == "f47ac10b-58cc-4372-a567-0e02b2c3d479" );
     
-//     session.set_header( "Connection", "close" );
-//     multimap< string, string > expectation = { { "Connection", "close" } };
-//     REQUIRE( session.get_headers( ) == expectation );
+    session.set_header( "Connection", "close" );
+    multimap< string, string > expectation = { { "Connection", "close" } };
+    REQUIRE( session.get_headers( ) == expectation );
     
-//     expectation =
-//     {
-//         { "Content-Type", "application/yaml" },
-//         { "Content-Encoding", "" }
-//     };
+    expectation =
+    {
+        { "Content-Type", "application/yaml" },
+        { "Content-Encoding", "" }
+    };
     
-//     session.set_headers( expectation );
-//     REQUIRE( session.get_headers( ) == expectation );
-// }
+    session.set_headers( expectation );
+    REQUIRE( session.get_headers( ) == expectation );
+}
 
-// TEST_CASE( "validate session context functionality", "[session]" )
-// {
-//     Session session( "f47ac10b-58cc-4372-a567-0e02b2c3d479" );
+TEST_CASE( "validate session context functionality", "[session]" )
+{
+    Session session( "f47ac10b-58cc-4372-a567-0e02b2c3d479" );
     
-//     session.set( "Connection", string( "close" ) );
-//     const string connection = session.get( "Connection" );
-//     REQUIRE( connection == "close" );
-//     REQUIRE( session.has( "Connection" ) );
-//     REQUIRE( not session.has( "connection" ) );
-//     REQUIRE( session.keys( ) == set< string >( { "Connection" } ) );
+    session.set( "Connection", string( "close" ) );
+    const string connection = session.get( "Connection" );
+    REQUIRE( connection == "close" );
+    REQUIRE( session.has( "Connection" ) );
+    REQUIRE( not session.has( "connection" ) );
+    REQUIRE( session.keys( ) == set< string >( { "Connection" } ) );
     
-//     session.erase( "Connection" );
-//     REQUIRE( not session.has( "Connection" ) );
+    session.erase( "Connection" );
+    REQUIRE( not session.has( "Connection" ) );
     
-//     const string keep_alive = session.get( "Connection", string( "keep-alive" ) );
-//     REQUIRE( keep_alive == "keep-alive" );
+    const string keep_alive = session.get( "Connection", string( "keep-alive" ) );
+    REQUIRE( keep_alive == "keep-alive" );
     
-//     REQUIRE( session.keys( ) == set< string >( ) );
+    REQUIRE( session.keys( ) == set< string >( ) );
     
-//     session.set( "Connection", string( "close" ) );
-//     session.set( "Connection", string( "keep-alive" ) );
+    session.set( "Connection", string( "close" ) );
+    session.set( "Connection", string( "keep-alive" ) );
     
-//     const string header = session.get( "Connection" );
-//     REQUIRE( header == "keep-alive" );
+    const string header = session.get( "Connection" );
+    REQUIRE( header == "keep-alive" );
     
-//     try
-//     {
-//         int type = session.get( "Connection" );
-//         REQUIRE( type == 0 );
-//     }
-//     catch ( const bad_cast& )
-//     {
-//         REQUIRE( true );
-//     }
+    try
+    {
+        int type = session.get( "Connection" );
+        REQUIRE( type == 0 );
+    }
+    catch ( const bad_cast& )
+    {
+        REQUIRE( true );
+    }
     
-//     session.erase( );
-//     REQUIRE( session.keys( ) == set< string >( ) );
+    session.erase( );
+    REQUIRE( session.keys( ) == set< string >( ) );
     
-//     REQUIRE_THROWS_AS( session.get( "Connection" ), out_of_range );
-// }
+    REQUIRE_THROWS_AS( session.get( "Connection" ), out_of_range );
+}
