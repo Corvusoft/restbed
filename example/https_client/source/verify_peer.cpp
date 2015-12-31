dba0a81119c836b8e5f13eb96b3f05cc99e9ea4b
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2015-12-31 17:33:19 +1300

    Improved HTTPS client instructions.

--- example/https_client/source/verify_peer.cpp
+++ example/https_client/source/verify_peer.cpp
@@ -1,24 +1,15 @@
 /*
  * Example illustrating a HTTPS client with peer verification.
  *
- * Get CA certifiate (http://gagravarr.org/writing/openssl-certs/general.shtml)
- * note the subject hash never seems to match?!? use dtruss/strace to see what it wants to load. Its also more reliable
- * to download the certificate straight from the CA (for google.com its https://www.geotrust.com/resources/root-certificates/)
+ * 1. Download the certificate authority for the desired peer.
+ *    You can find this information via 'openssl s_client -connect www.google.com:443 -showcerts'
+ * 2. Test you have the correct certificate 'openssl s_client -connect www.google.com:443 -showcerts -CAfile Equifax_Secure_Certificate_Authority.pem'
+ * 3. OpenSSL requires a link to the certiciate using the subject hash and a '.0' extension.
+ *    The hash can be generated using 'openssl x509 -in Equifax_Secure_Certificate_Authority.pem -noout -subject_hash'
+ * 4. ln -s Equifax_Secure_Certificate_Authority.pem 578d5c04.0
+ * 5. Verify your setup 'openssl s_client -showcerts  -CApath ./DIRECTORY_WHERE_CERTFICIATE_LIVES -connect google.com:443'
+ * 6. Make sure the file permissions are correct for the CA certificate
  *
- * http://stackoverflow.com/questions/23343910/verify-errornum-20-when-connecting-to-gateway-sandbox-push-apple-com
- * 1. openssl s_client -connect www.google.com:443 -showcerts
- * 2. download root CA
- * 3. test root CA, openssl s_client -connect www.google.com:443 -showcerts -CAfile ./certs/Equifax_Secure_Certificate_Authority.pem
- * Each file in the directory must contain a single certificate.
- * The files must be named using the subject name's hash and an extension of ".0".
- * 4. openssl x509 -in certs/Equifax_Secure_Certificate_Authority.pem -noout -subject_hash (also see c_rehash)
- * can be used to generate the subject name hash(e.g. e5d93f80). Just append ".0" to this value and create a softlink with this name(e5d93f80.0) to the CA file. If there are more than one CA files with same the subject name hash value, their extensions should be different(e.g. e5d93f80.1). The search is performed in the ordering of the extension number.
- * opensssl has introduced this technique to reduce the the CA file look up time. Otherwise openssl may have to read all files in the CApath to find the matching CA file.
- * 5. ln -s Equifax_Secure_Certificate_Authority.pem 594f1775.0
- * 6. verify its all setup correct, openssl s_client -showcerts  -CApath ./certs -connect google.com:443
- * 7. make sure the file permissions are correct for the CA root cert (http://stackoverflow.com/questions/15936668/boostasiosslcontextadd-verify-path)
- *
- * to confirm loading of the right hash use strace or dtruss (on mac) to verifiy the hash file being loaded.
  * Usage:
  *    ./distribution/example/https_client_verify_peer
  */
