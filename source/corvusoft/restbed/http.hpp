c1c6199fc1a0c19d7e50af9da5744534697e6db9
Callum Kirby <callum.kirby@corvusoft.co.uk>
2016-06-16 16:12:16 +1200

    Resolved issue with destructor call order.

--- source/corvusoft/restbed/http.hpp
+++ source/corvusoft/restbed/http.hpp
@@ -50,9 +50,9 @@ namespace restbed
             
             static bool is_closed( const std::shared_ptr< Request >& request );
             
-            static const std::shared_ptr< Response > sync( const std::shared_ptr< Request >& request, const std::shared_ptr< const Settings >& settings = std::make_shared< Settings >( ) );
+            static const std::shared_ptr< Response > sync( const std::shared_ptr< Request > request, const std::shared_ptr< const Settings >& settings = std::make_shared< Settings >( ) );
             
-            static std::future< std::shared_ptr< Response > > async( const std::shared_ptr< Request >& request, const std::function< void ( const std::shared_ptr< Request >, const std::shared_ptr< Response > ) >& callback, const std::shared_ptr< const Settings >& settings = std::make_shared< Settings >( ) );
+            static std::future< std::shared_ptr< Response > > async( const std::shared_ptr< Request > request, const std::function< void ( const std::shared_ptr< Request >, const std::shared_ptr< Response > ) >& callback, const std::shared_ptr< const Settings >& settings = std::make_shared< Settings >( ) );
             
             static Bytes fetch( const std::size_t length, const std::shared_ptr< Response >& response );
             
