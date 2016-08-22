cb9bb7ad585af6f020260f85d6882dffbf3f99cf
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2016-08-23 09:30:49 +1200

    Resolved issue with off-by-one path parameters.

--- source/corvusoft/restbed/detail/service_impl.cpp
+++ source/corvusoft/restbed/detail/service_impl.cpp
@@ -550,7 +550,13 @@ namespace restbed
             smatch matches;
             static const regex pattern( "^\\{([a-zA-Z0-9]+): ?.*\\}$" );
             
-            const auto folders = String::split( request->get_path( ), '/' );
+            auto folders = String::split( request->get_path( ), '/' );
+            
+            if ( m_settings->get_root( ) not_eq "/" )
+            {
+                folders.erase( folders.begin( ) );
+            }
+            
             const auto declarations = String::split( m_resource_paths.at( sanitised_path ), '/' );
             
             for ( size_t index = 0; index < folders.size( ) and index < declarations.size( ); index++ )
