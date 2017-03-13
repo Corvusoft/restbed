fe9435eedf73f80f331754ce6add1aed8c3a7ab1
Ivan Shynkarenka <chronoxor@gmail.com>
2017-03-14 00:22:45 +0300

    Allow to use external io_service in restbed::Service

--- source/corvusoft/restbed/service.hpp
+++ source/corvusoft/restbed/service.hpp
@@ -16,6 +16,7 @@
 //Project Includes
 
 //External Includes
+#include <asio/io_service.hpp>
 
 //System Namespaces
 
@@ -82,6 +83,8 @@ namespace restbed
             //Setters
             void set_logger( const std::shared_ptr< Logger >& value );
             
+            void set_external_io_service( std::shared_ptr< asio::io_service >& value );
+
             void set_session_manager( const std::shared_ptr< SessionManager >& value );
             
             void set_ready_handler( const std::function< void ( Service& ) >& value );
