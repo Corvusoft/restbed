9a78249a3d78d395a9d6cfeb0ba2f56e4092e467
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2017-07-29 11:37:48 +1000

    Resolved issue #223.

--- source/corvusoft/restbed/detail/http_impl.cpp
+++ source/corvusoft/restbed/detail/http_impl.cpp
@@ -239,7 +239,7 @@ namespace restbed
             getline( response_stream, status_line );
             
             smatch matches;
-            static const regex status_line_pattern( "^([a-zA-Z]+)\\/(\\d*\\.?\\d*) (-?\\d+) (.+)\\r$" );
+            static const regex status_line_pattern( "^([a-zA-Z]+)\\/(\\d*\\.?\\d*) (-?\\d+) (.*)\\r$" );
             
             if ( not regex_match( status_line, matches, status_line_pattern ) or matches.size( ) not_eq 5 )
             {
