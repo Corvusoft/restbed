f25b07ceb76ab7cfe4c33b839d33eff4bbf52748
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2018-04-08 16:52:12 +1000

    Upgraded OpenSSL dependency.

--- cmake/Findopenssl.cmake
+++ cmake/Findopenssl.cmake
@@ -1,8 +1,12 @@
-find_library( ssl_LIBRARY ssl ssleay32 HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/out32dll" "${PROJECT_SOURCE_DIR}/dependency/openssl" "/usr/local/opt/openssl/lib" "/usr/lib" "/usr/local/lib" "/opt/local/lib" )
-find_library( crypto_LIBRARY crypto libeay32 HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/out32dll" "${PROJECT_SOURCE_DIR}/dependency/openssl" "/usr/local/opt/openssl/lib" "/usr/lib" "/usr/local/lib" "/opt/local/lib" )
+find_library( ssl_LIBRARY_STATIC libssl.a  ssleay32.lib HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/out32dll" "${PROJECT_SOURCE_DIR}/dependency/openssl" "/usr/local/opt/openssl/lib" "/usr/lib" "/usr/local/lib" "/opt/local/lib" )
+find_library( crypto_LIBRARY_STATIC libcrypto.a libeay32.lib HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/out32dll" "${PROJECT_SOURCE_DIR}/dependency/openssl" "/usr/local/opt/openssl/lib" "/usr/lib" "/usr/local/lib" "/opt/local/lib" )
+
+find_library( ssl_LIBRARY_SHARED libssl.so libssl.dylib ssleay32.dll HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/out32dll" "${PROJECT_SOURCE_DIR}/dependency/openssl" "/usr/local/opt/openssl/lib" "/usr/lib" "/usr/local/lib" "/opt/local/lib" )
+find_library( crypto_LIBRARY_SHARED libcrypto.so libcrypto.dylib libeay32.dll HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/out32dll" "${PROJECT_SOURCE_DIR}/dependency/openssl" "/usr/local/opt/openssl/lib" "/usr/lib" "/usr/local/lib" "/opt/local/lib" )
+
 find_path( ssl_INCLUDE openssl/ssl.h HINTS "${PROJECT_SOURCE_DIR}/dependency/openssl/inc32" "${PROJECT_SOURCE_DIR}/dependency/openssl/include" "/usr/local/opt/openssl/include" "/usr/include" "/usr/local/include" "/opt/local/include" )
 
-if ( ssl_INCLUDE AND ssl_LIBRARY AND crypto_LIBRARY )
+if ( ssl_LIBRARY_STATIC AND ssl_LIBRARY_SHARED AND crypto_LIBRARY_STATIC AND crypto_LIBRARY_SHARED )
     set( OPENSSL_FOUND TRUE )
     add_definitions( -DBUILD_SSL=TRUE )
 
@@ -10,9 +14,11 @@ if ( ssl_INCLUDE AND ssl_LIBRARY AND crypto_LIBRARY )
         set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations" )
     endif( )
 
-    message( STATUS "Found OpenSSL library at: ${ssl_LIBRARY}" )
     message( STATUS "Found OpenSSL include at: ${ssl_INCLUDE}" )
-    message( STATUS "Found Crypto library at: ${crypto_LIBRARY}" )
+    message( STATUS "Found OpenSSL library at: ${ssl_LIBRARY_STATIC}" )
+    message( STATUS "Found OpenSSL library at: ${ssl_LIBRARY_SHARED}" )
+    message( STATUS "Found Crypto library at: ${crypto_LIBRARY_STATIC}" )
+    message( STATUS "Found Crypto library at: ${crypto_LIBRARY_SHARED}" )
 else ( )
     message( FATAL_ERROR "Failed to locate OpenSSL dependency. see restbed/dependency/openssl; ./config shared; make all" )
 endif ( )
