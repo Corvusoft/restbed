3040d12c2aa9ca032976e62abe678fcefde68fd1
Ben Crowhurst <ben.crowhurst@corvusoft.co.uk>
2018-04-08 13:03:13 +1000

    Removed kashmir dependency.

--- cmake/modules/Findkashmir.cmake
+++
@@ -1,9 +0,0 @@
-find_path( kashmir_INCLUDE kashmir HINTS "${PROJECT_SOURCE_DIR}/dependency/kashmir" "/usr/include" "/usr/local/include" "/opt/local/include" )
-
-if ( kashmir_INCLUDE )
-    set( KASHMIR_FOUND TRUE )
-
-    message( STATUS "Found Kashmir include at: ${kashmir_INCLUDE}" )
-else ( )
-    message( FATAL_ERROR "Failed to locate Kashmir dependency." )
-endif ( )
