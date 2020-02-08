207277d5e01d5537d87b28549e93edda1a173486
Baptiste Jonglez <git@bitsofnetworks.org>
2020-02-08 12:45:39 +0100

    cmake: Auto-detect Catch dependency to decide whether to enable tests
    This automatically enables or disables tests, depending on whether Catch
    is found on the system.
    
    It is still possible to force building tests or not:
    
      * `-DBUILD_TESTS=OFF` will disable tests even if Catch is found
      * `-DBUILD_TESTS=ON` will enable tests even if Catch is not found (mostly useful because it causes cmake to fail early)

--- cmake/Findcatch.cmake
+++ cmake/Findcatch.cmake
@@ -1,9 +1,4 @@
 find_path( catch_INCLUDE catch.hpp HINTS "${PROJECT_SOURCE_DIR}/dependency/catch/single_include/catch2" "/usr/include/catch2/single_include" "/usr/include/catch2" "/usr/local/include/catch2/single_include" "/usr/local/include/catch2" "/opt/local/include/catch2/single_include" "/opt/local/include/catch2" )
 
-if ( catch_INCLUDE )
-    set( CATCH_FOUND TRUE )
-
-    message( STATUS "Found Catch include at: ${catch_INCLUDE}" )
-else ( )
-    message( FATAL_ERROR "Failed to locate Catch dependency." )
-endif ( )
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(catch DEFAULT_MSG catch_INCLUDE)
