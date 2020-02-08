1db9d8785fd287a7a59565610346cb08cf541cbd
Baptiste Jonglez <git@bitsofnetworks.org>
2020-02-08 12:24:25 +0100

    tests: fix include path for Catch2
    When installing Catch2 through CMake or pkg-config, the header file is in
    /usr/include/catch2 or the /usr/local equivalent, see the release notes:
    
    https://github.com/catchorg/Catch2/releases/tag/v2.3.0
    
    Fix the include paths accordingly.

--- cmake/Findcatch.cmake
+++ cmake/Findcatch.cmake
@@ -1,4 +1,4 @@
-find_path( catch_INCLUDE catch.hpp HINTS "${PROJECT_SOURCE_DIR}/dependency/catch/single_include/catch2" "/usr/include/catch2/single_include" "/usr/local/include/catch2/single_include" "/opt/local/include/catch2/single_include" )
+find_path( catch_INCLUDE catch.hpp HINTS "${PROJECT_SOURCE_DIR}/dependency/catch/single_include/catch2" "/usr/include/catch2/single_include" "/usr/include/catch2" "/usr/local/include/catch2/single_include" "/usr/local/include/catch2" "/opt/local/include/catch2/single_include" "/opt/local/include/catch2" )
 
 if ( catch_INCLUDE )
     set( CATCH_FOUND TRUE )
